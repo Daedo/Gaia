e41616c0961e1e4296ce2cf3d2e2edd5389090ae
Daedo <daedo@daedo-play.de>
2017-12-27 12:01:39 +0100

    Code cleanup

--- src/Bindings.js
+++ src/Bindings.js
@@ -7,6 +7,7 @@ $(".tablinks").click(function (evt) {
   if (tabName === "save") createSavefile();
   openTab(evt, tabName);
 });
+
 // Star handlers
 $("#addStar").click(function (evt) {
   starEditor.addStar();
@@ -17,9 +18,7 @@ $("#deleteStar").click(function (evt) {
 $("#star-name").on("input", function (evt) {
   starEditor.updateName(evt.target.value);
 });
-/*$("#star-mass").on("input", function (evt) {
-  starEditor.updateMass(evt.target.value);
-});*/
+
 // Planet handlers
 $("#addPlanet").click(function (evt) {
   planetEditor.addPlanet();
@@ -30,15 +29,10 @@ $("#deletePlanet").click(function (evt) {
 $("#planet-name").on("input", function (evt) {
   planetEditor.updateName(evt.target.value);
 });
-/*$("#planet-mass").on("input", function (evt) {
-  planetEditor.updateMass(evt.target.value);
-});
-$("#planet-radius").on("input", function (evt) {
-  planetEditor.updateRadius(evt.target.value);
-});*/
 $("#planet-typeselector").change(function (evt) {
   planetEditor.updatePlanetType(evt.target.value);
 });
+
 // Orbit handlers
 $("#addOrbit").click(function (evt) {
   orbitEditor.addOrbit();
@@ -52,21 +46,7 @@ $("#orbit-starselector").change(function (evt) {
 $("#object-planetselector").change(function (evt) {
   orbitEditor.updatePlanetIndex(evt.target.value);
 });
-/*$("#object-semi-major-axis").on("input", function (evt) {
-  orbitEditor.updateSemiMajorAxis(evt.target.value);
-});
-$("#object-eccentricity").on("input", function (evt) {
-  orbitEditor.updateEccentricity(evt.target.value);
-});
-$("#object-inclination").on("input", function (evt) {
-  orbitEditor.updateInclination(evt.target.value);
-});
-$("#object-longitude-of-the-ascending-node").on("input", function (evt) {
-  orbitEditor.updateLongitudeOfTheAscendingNode(evt.target.value);
-});
-$("#object-argument-of-periapsis").on("input", function (evt) {
-  orbitEditor.updateArgumentOfPeriapsis(evt.target.value);
-});*/
+
 $("#addObject").click(function (evt) {
   orbitEditor.addObject();
 });
@@ -89,33 +69,6 @@ $("#moon-name").on("input", function (evt) {
 $("#moon-typeselector").change(function (evt) {
   moonEditor.updateMoonType(evt.target.value);
 });
-/*$("#moon-mass").on("input", function (evt) {
-  moonEditor.updateMass(evt.target.value);
-});
-$("#moon-radius").on("input", function (evt) {
-  moonEditor.updateRadius(evt.target.value);
-});
-$("#moon-radius-2").on("input", function (evt) {
-  moonEditor.updateRadiusB(evt.target.value);
-});
-$("#moon-radius-3").on("input", function (evt) {
-  moonEditor.updateRadiusC(evt.target.value);
-});
-$("#moon-semi-major-axis").on("input", function (evt) {
-  moonEditor.updateSemiMajorAxis(evt.target.value);
-});
-$("#moon-eccentricity").on("input", function (evt) {
-  moonEditor.updateEccentricity(evt.target.value);
-});
-$("#moon-inclination").on("input", function (evt) {
-  moonEditor.updateInclination(evt.target.value);
-});
-$("#moon-longitude-of-the-ascending-node").on("input", function (evt) {
-  moonEditor.updateLongitudeOfTheAscendingNode(evt.target.value);
-});
-$("#moon-argument-of-periapsis").on("input", function (evt) {
-  moonEditor.updateArgumentOfPeriapsis(evt.target.value);
-});*/
 $("#addMoon").click(function (evt) {
   moonEditor.addMoon();
 });
